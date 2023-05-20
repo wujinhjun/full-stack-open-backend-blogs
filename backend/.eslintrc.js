b2ad7bab801383eb9e0f335d2e022e72aeacc29c
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-20 22:59:24 +0800

    chore: config the eslint

---
+++ backend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-unused-vars": "off",
+  },
+};
