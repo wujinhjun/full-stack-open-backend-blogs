3de4b907fe3ea2d308382974b649b01cbcfb577f
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-21 16:00:45 +0800

    chore: config the environment variable

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,5 +1,12 @@
+require("dotenv").config();
 const PORT = process.env.PORT ?? 3003;
-const MONGODB_URI = `mongodb+srv://blogs:blogs-backend@blogs-backend.issmnpg.mongodb.net/?retryWrites=true&w=majority`;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
+// const MONGODB_URI = process.env.TEST_MONGODB_URI;
+
+console.log(MONGODB_URI);
 
 module.exports = {
   PORT,
