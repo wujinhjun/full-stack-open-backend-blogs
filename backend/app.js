fa8c19007677d1cd37b018d48e2026d8d7085943
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-23 10:29:11 +0800

    feat: update the config

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const middleware = require("./utils/middleware");
+require("express-async-errors");
 
 const config = require("./utils/config");
-const Blog = require("./models/blog");
-
 const blogsRouter = require("./controllers/blogsRouter");
+
 const app = express();
+
+mongoose.set("strictQuery", false);
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -20,11 +23,16 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 morgan.token("body", (req, res) => JSON.stringify(req.body) ?? "");
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
