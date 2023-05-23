74d22a5bc67931b728f6dd368caa16bb81b76b71
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-23 10:26:31 +0800

    refactor: update the validator

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  url: { type: String, required: true },
+  likes: {
+    type: Number,
+    default: 0,
+    validate: {
+      validator: (value) => {
+        return value >= 0;
+      },
+      message: (props) => `The ${props.value} is not a valid likes count`,
+    },
+  },
 });
 
 blogSchema.set("toJSON", {
