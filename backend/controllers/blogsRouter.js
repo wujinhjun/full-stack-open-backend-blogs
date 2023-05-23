30a4a387810f37ddc97059c32c7010ca51c3d2ab
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-23 10:25:13 +0800

    refactor: async/await to replace the promise link

--- backend/controllers/blogsRouter.js
+++ backend/controllers/blogsRouter.js
@@ -2,18 +2,51 @@ const blogsRouter = require("express").Router();
 
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
+blogsRouter.get("/", async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({});
     response.json(blogs);
-  });
+  } catch (error) {
+    next(error);
+  }
 });
 
-blogsRouter.post("/", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
+blogsRouter.post("/", async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body);
+    const result = await blog.save();
     response.status(201).json(result);
-  });
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogsRouter.delete("/:id", async (request, response, next) => {
+  console.log("delete");
+  try {
+    const result = await Blog.findByIdAndDelete(request.params.id);
+    console.log(result);
+    response.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogsRouter.put("/:id", async (request, response, next) => {
+  const id = request.params.id;
+
+  try {
+    const oldBlog = await Blog.findById(id);
+    const newBlog = { ...oldBlog, ...request.body };
+    const updatedBlog = await Blog.findByIdAndUpdate(id, newBlog, {
+      new: true,
+      runValidators: true,
+      context: "query",
+    });
+    response.json(updatedBlog);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = blogsRouter;
