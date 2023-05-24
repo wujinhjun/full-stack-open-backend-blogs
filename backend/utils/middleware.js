200779bdf7af874159153344cfe6e4c5aa9b795e
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-24 14:12:16 +0800

    feat: update the middleware

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -1,4 +1,8 @@
+const jwt = require("jsonwebtoken");
 const logger = require("./logger");
+const User = require("../models/user");
+
+const SECRET = require("../utils/config").SECRET;
 
 const requestLogger = (request, response, next) => {
   logger.info("Method:", request.method);
@@ -19,13 +23,43 @@ const errorHandler = (err, req, res, next) => {
     return res
       .status(400)
       .json({ error: err.message, type: "ValidationError" });
+  } else if (err.name === "JsonWebTokenError") {
+    if (req.token === null) {
+      return res.status(401).json({ error: err.message });
+    }
+    return res.status(400).json({ error: err.message });
   }
 
   next(err);
+};
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    request.token = authorization.replace("Bearer ", "");
+    // next();
+  } else {
+    // next(new Error());
+    request.token = null;
+  }
+  next();
+};
+
+const userExtractor = async (request, response, next) => {
+  try {
+    const decodedToken = jwt.verify(request.token, SECRET);
+    const user = await User.findById(decodedToken.id);
+    request.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
+  userExtractor,
 };
