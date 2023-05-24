f92100f33cfc73eb10d1e78d17c87c71d31feadb
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-24 14:12:05 +0800

    feat: update the config

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const PORT = process.env.PORT ?? 3003;
+const SECRET = process.env.SECRET;
 const MONGODB_URI =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
 // const MONGODB_URI = process.env.TEST_MONGODB_URI;
 
-console.log(MONGODB_URI);
-
 module.exports = {
   PORT,
   MONGODB_URI,
+  SECRET,
 };
