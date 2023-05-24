d0048ee4c40dea334cd3cbaaa4d783d5fcc7089b
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-24 14:11:16 +0800

    feat: create user

---
+++ backend/controllers/usersRouter.js
@@ -0,0 +1,30 @@
+const usersRouter = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    likes: 1,
+    url: 1,
+  });
+  response.json(users);
+});
+
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
