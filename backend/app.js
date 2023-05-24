6ae4d79f4d93c8b3343f1b826053d4aa2e4fc425
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-24 14:12:39 +0800

    feat: add some configurable for app

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,10 @@ const middleware = require("./utils/middleware");
 require("express-async-errors");
 
 const config = require("./utils/config");
+const logger = require("./utils/logger");
 const blogsRouter = require("./controllers/blogsRouter");
+const usersRouter = require("./controllers/usersRouter");
+const loginRouter = require("./controllers/loginRouter");
 
 const app = express();
 
@@ -15,10 +18,10 @@ mongoose.set("strictQuery", false);
 mongoose
   .connect(config.MONGODB_URI)
   .then((result) => {
-    console.log("connected to MongoDB");
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    console.log(`error connecting to MongoDB: ${error.message}`);
+    logger.info(`error connecting to MongoDB: ${error.message}`);
   });
 
 app.use(cors());
@@ -29,8 +32,12 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
+// app.use(middleware.userExtractor);
 
+app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
