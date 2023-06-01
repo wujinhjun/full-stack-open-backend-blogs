58846d8deb0abb334931670490a36910d7845dd2
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-06-01 16:50:50 +0800

    feat: net service for blog

---
+++ frontend/src/services/blogs.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+const baseUrl = "/api/blogs";
+let token = "";
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
+
+const createBlog = async (newBlog) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.post(baseUrl, newBlog, config);
+  return response.data;
+};
+
+const updateBlog = async (id, newBlog) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.put(`${baseUrl}/${id}`, newBlog, config);
+  return response.data;
+};
+
+const removeBlog = async (id) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.delete(`${baseUrl}/${id}`, config);
+  return response.data;
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getAll, createBlog, setToken, updateBlog, removeBlog };
